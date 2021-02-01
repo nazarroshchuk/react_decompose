f9937c52d84b8d83481f5a00f346dcff58290c6a
Nazarii <nazarrishchuk@gmail.com>
2021-02-01 14:46:33 +0200

    Updates

---
+++ precache-manifest.686a0f45f08426c0aa7ecefa3cd5dc4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01e879f739fbe73e8f53ae41fce122c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "823737f4baf758fc3b5a",
+    "url": "./static/css/main.62c59f10.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "823737f4baf758fc3b5a",
+    "url": "./static/js/main.1ddb4b19.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
